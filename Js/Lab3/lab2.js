b5bfa105d51be5f50ecdb44fd3c0326c7873995c
LuciyL <126418252+LuciyL@users.noreply.github.com>
2023-06-04 17:35:27 +0300

    qwe

--- Js/Lab3/lab2.js
+++ Js/Lab3/lab2.js
@@ -1,58 +1,58 @@
-function pow(x, n) {
-    return x ** n;
+
+function pow(x, n)
+{
+    return x**n; 
 }
 
-function sumTo(n) {
-   let sum = 0;
-   for (let i = 1; i <= n; i++) {
-       sum += i
-   }
-   return sum;
+function sumTo(n)
+{
+    return (n*(n+1))/2  
+
 }
 
-   
-function factorial(n) {   
-   if (n<=1) return 1n;
-   else {
-       res = BigInt (n) * BigInt(factorial(n-1))
-       return res;
-   }  
+function factorial(n)
+{
+    if (n<=0)
+    {
+        return 1n; 
+    }
+    else{
+        return ( BigInt (n) *  BigInt(factorial(n-1))); 
+    }
 }
- function fib(n){
-   a = BigInt(1); 
-   b=BigInt(1); 
-   if (n== BigInt(0))
-   {
-       return BigInt(0); 
-   }
-   else
-   { 
-       for (i = BigInt(3); i<=n; i++) 
-       {
-           c = a+b;
-           a=b;
-           b=c;
-       }
-       return b;
-   }
+
+export function fib(n) {
+    let a = 1n;
+    let b = 1n;
+    if (n == 0n) {
+        return 0n;
+    }
+    else {
+        for (let i = 3n; i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 }
 
 function compare(x)
 {
 return function(y)
 {
-   if (y>x) return true;
-   else if (y<x) return false;
-   else if (y==x) return null;
+    if (y>x) return true;
+    else if (y<x) return false;
+    else if (y==x) return null;
 }
 }
 
 function sum()
 {
-   var result=0; 
-   for (var i=0; i<arguments.length; i++)
-   {
-       result+=arguments[i] 
-   }
-   return result;
+    var result=0; 
+    for (var i=0; i<arguments.length; i++)
+    {
+        result+=arguments[i] 
+    }
+    return result;
 }
